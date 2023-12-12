1ef99316b708c0b2860a2d4cda044c5ba8e10ba6
Derrick Gozal <derrickgozal@gmail.com>
2023-12-12 00:18:53 -0500

    1.7

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,8 @@ author = u"Derrick Gozal"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+import myst_nb
+
 extensions = [
     "myst_nb",
     "autoapi.extension",
